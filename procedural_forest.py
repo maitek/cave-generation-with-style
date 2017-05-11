85836a5da1a0c269111b728b602db6e623686107
Mathias Sundholm <mathias_sundholm@hotmail.com>
2017-05-11 02:37:34 +0200

    Cleaned up procedural forest code

--- procedural_forest.py
+++ procedural_forest.py
@@ -5,11 +5,9 @@ from numpy.linalg import norm
 map_size = (100,100)
 num_trees = 500
 random_pos = np.random.rand(num_trees,2)
-#import pdb; pdb.set_trace()
-#plt.plot(random_pos,".g")
-#plt.show()
 
-
+plt.plot(random_pos,".g")
+plt.show()
 
 def check_valid(new_point,points):
     for point in points:
@@ -26,7 +24,8 @@ new_point = np.zeros((1,2))
 points.append(new_point)
 edge_points.append(new_point)
 
-while num_points < 1000 and len(edge_points) > 0:
+# Generate procedural forest by adding trees to the edge of the forest
+while num_points < num_trees and len(edge_points) > 0:
     # select random edge
     idx = np.random.randint(0,len(edge_points))
     seed_point = edge_points[idx]
@@ -68,14 +67,3 @@ while num_points < 1000 and len(edge_points) > 0:
             #plt.plot(seed_point_array[0],seed_point_array[1],".r")
             #plt.plot(attempts_array[:,0],attempts_array[:,1],".b")
             plt.show()
-
-
-
-# while(num_points < max_points):
-    # add point at current_pos, and mark it edge
-    # num_points += 1
-    # select a random edge point
-        # if is_still_edge():
-            # current_pos = create_new_pos(current_pos)
-        # else:
-            # mark as not edge
